e8f5225b253004691ae9e9c54c707045e71e8dab
AlWaleed Musa <jako_249@hotmail.com>
2024-01-02 13:32:08 -0800

    refacorint code

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -11,6 +11,7 @@ elif int(last_digit) < 6 and int(last_digit) != 0:
     if int(last_digit) < 0:
         nlast_digit = int(last_digit) * -1
         print(f"Last digit of {string} is {nlast_digit} and is less than 6 and not 0")
-    print(f"Last digit of {string} is {last_digit} and is less than 6 and not 0")
+    else:
+        print(f"Last digit of {string} is {last_digit} and is less than 6 and not 0")
 elif int(last_digit) == 0:
     print(f"Last digit of {string} is {last_digit} and is 0")
