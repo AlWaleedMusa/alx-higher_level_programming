108dbaa0e70b2d35d2bd05e2a4191deeb313fe84
AlWaleed Musa <jako_249@hotmail.com>
2024-01-02 14:15:27 -0800

    refacorint code

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-	convert = str(number)
-	last_digit = convert[-1]
-	print(last_digit, end="")
-	return (last_digit)
+    convert = str(number)
+    last_digit = convert[-1]
+    print(last_digit, end="")
+    return (last_digit)
