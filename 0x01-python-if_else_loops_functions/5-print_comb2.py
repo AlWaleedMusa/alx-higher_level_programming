2cf7f0972667103fc9efaeec73cdb2a216111374
AlWaleed Musa <jako_249@hotmail.com>
2024-01-02 14:02:38 -0800

    refacorint code

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 for i in range(100):
-	if i < 10:
-		print("0", end="")
-	if i == 99:
-		print(i, end="\n")
-	else:
-		print(i, end=", ")
+    if i < 99:
+        print("{:02d}".format(i), end=", ")
+    else:
+        print("{:d}".format(i))
