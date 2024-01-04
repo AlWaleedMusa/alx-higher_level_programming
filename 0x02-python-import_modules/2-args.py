d61f53a0be27dcc7d52cc57dba0ac3565247e937
AlWaleed Musa <jako_249@hotmail.com>
2024-01-04 08:40:52 -0800

    refactoring code

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,10 +4,12 @@ import sys
 
 if __name__ == "__main__":
     length = len(sys.argv)
-    if length < 1:
-        print("0 arguments.")
-    elif length > 1:
-        print("{} arguments: ".format(length - 1))
+    if length == 1:
+        print("{} arguments.".format(length - 1))
+    elif length == 2:
+        print("{} argument:".format(length - 1))
+    else:
+        print("{} arguments:".format(length - 1))
 
     for i, value in enumerate(range(1, length)):
         print("{}: {}".format(i + 1, sys.argv[i + 1]))
