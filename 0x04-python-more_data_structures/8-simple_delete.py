87784320b16c48689b37b64498d828b2c383bf98
AlWaleed Musa <jako_249@hotmail.com>
2024-01-10 08:44:39 -0800

    Task bug fixed

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def simple_delete(a_dictionary, key=""):
-    if key:
+    if key in a_dictionary.keys():
         a_dictionary.pop(key)
         return a_dictionary
     return (a_dictionary)
