12b8acfc1480f4687efa37b1befdddf85e18aab1
AlWaleed Musa <jako_249@hotmail.com>
2024-01-10 14:18:17 -0800

    task 12 bug fixed

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 
 def roman_to_int(roman_string):
-    if not roman_string:
-        return 0
-    if isinstance(roman_string, str):
+
+    if not roman_string or type(roman_string) != str:
         return 0
 
     simps = [x for x in roman_string]
@@ -23,7 +22,5 @@ def roman_to_int(roman_string):
             num += 500
         elif simp == "M":
             num += 1000
-        else:
-            num += 0
 
     return num
