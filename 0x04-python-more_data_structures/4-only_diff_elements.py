04b5c1757a4feb674a41b29cc19c1afdc0185210
AlWaleed Musa <jako_249@hotmail.com>
2024-01-10 07:19:01 -0800

    Task 4 done

---
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def only_diff_elements(set_1, set_2):
+    new = set()
+    for num in set_1:
+        if num in set_2:
+            continue
+        new.add(num)
+    for num in set_2:
+        if num in set_1:
+            continue
+        new.add(num)
+    return(new)
+
