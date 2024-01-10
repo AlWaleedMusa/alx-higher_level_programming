9b9f46d7e893b2bedf6782dbb938a208dc80400e
AlWaleed Musa <jako_249@hotmail.com>
2024-01-10 14:06:56 -0800

    task 12 done

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+def roman_to_int(roman_string):
+    if type(roman_string) != str or roman_string == None:
+        return 0
+
+    simps = [x for x in roman_string]
+    num = 0
+    for simp in simps:
+        if simp == "I":
+            num += 1
+        elif simp == "V":
+            num += 5
+        elif simp == "X":
+            num += 10
+        elif simp == "L":
+            num += 50
+        elif simp == "C":
+            num += 100
+        elif simp == "D":
+            num += 500
+        elif simp == "M":
+            num += 1000
+
+    return num
