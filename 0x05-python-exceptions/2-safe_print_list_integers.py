4e790b29793df49e31e26833033379002a5167d9
AlWaleed Musa <jako_249@hotmail.com>
2024-01-22 20:05:31 +0200

    refactoring code

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -3,9 +3,9 @@
 def safe_print_list_integers(my_list=[], x=0):
 	if my_list is None:
 		return 0
-	
+
+	counter = 0
 	try:
-		counter = 0
 		for i in range(x):
 			if type(my_list[i]) == int:
 				print("{:d}".format(my_list[i]), end="")
