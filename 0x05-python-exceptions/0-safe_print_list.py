6bed96e3123fde08b0eb016b9648cebfd11ab809
AlWaleed Musa <jako_249@hotmail.com>
2024-01-22 16:20:46 +0200

    pycode style check

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,16 +2,16 @@
 
 def safe_print_list(my_list=[], x=0):
 
-	if my_list == None:
-		return 0
+    if my_list == None:
+        return 0
 
-	counter = 0
-	try:
-		for i in range(x):
-			print(my_list[i], end="")
-			counter += 1
-		print()
-	except IndexError:
-		print()
-		pass
-	return counter
+    counter = 0
+    try:
+        for i in range(x):
+            print(my_list[i], end="")
+            counter += 1
+        print()
+    except IndexError:
+        print()
+        pass
+    return counter
