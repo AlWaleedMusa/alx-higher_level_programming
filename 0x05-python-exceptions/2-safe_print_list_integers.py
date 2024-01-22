193b1029c43e18846cf012fb2d5e4527c05ba6ef
AlWaleed Musa <jako_249@hotmail.com>
2024-01-22 20:11:46 +0200

    pycode styleing

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 
 def safe_print_list_integers(my_list=[], x=0):
-	if my_list is None:
-		return 0
+    if my_list is None:
+        return 0
 
-	counter = 0
-	for i in range(x):
-		try:
-			if type(my_list[i]) == int:
-				print("{:d}".format(my_list[i]), end="")
-				counter += 1
-		except (ValueError, TypeError):
-			pass
-	print()
-	return counter
+    counter = 0
+    for i in range(x):
+        try:
+            if type(my_list[i]) == int:
+                print("{:d}".format(my_list[i]), end="")
+                counter += 1
+        except (ValueError, TypeError):
+            pass
+    print()
+    return counter
