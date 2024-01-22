3b4d13fd89927bf9b5b85735c893c63deebfd983
AlWaleed Musa <jako_249@hotmail.com>
2024-01-22 23:11:37 +0200

    pycode styling

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -2,9 +2,9 @@
 import sys
 
 def safe_print_integer_err(value):
-	try:
-		print("{:d}".format(value))
-		return True
-	except Exception as error:
-		print("Exception: {}".format(error), file=sys.stderr)
-		return False
+    try:
+        print("{:d}".format(value))
+        return True
+    except Exception as error:
+        print("Exception: {}".format(error), file=sys.stderr)
+        return False
