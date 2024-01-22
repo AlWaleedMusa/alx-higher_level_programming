fd618a61739dc079ce43e423afe7518fc374e17d
AlWaleed Musa <jako_249@hotmail.com>
2024-01-22 16:14:01 +0200

    refactoring code

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,11 +2,16 @@
 
 def safe_print_list(my_list=[], x=0):
 
+	if not my_list:
+		return 0
+
 	counter = 0
 	try:
 		for i in range(x):
 			print(my_list[i], end="")
 			counter += 1
+		print()
 	except IndexError:
+		print()
 		pass
 	return counter
