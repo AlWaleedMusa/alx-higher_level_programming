d7596473caf32d04d1ed204a0eab163e68e12019
AlWaleed Musa <jako_249@hotmail.com>
2024-01-23 22:03:31 +0200

    task 3 DONE

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" Creates a class Square
+"""
+
+
+class Square:
+    """ Empty class with size attribute
+    """
+    def __init__(self, size=0):
+        """
+            Instantiation with size
+
+        Args:
+            size: size of the square
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
+
+    def area(self):
+        """ return current square area
+        """
+        return (self.__size * self.__size)
