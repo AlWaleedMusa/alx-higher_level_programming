51dac64e3728f536b956f489c763b4c430caeb46
AlWaleed Musa <jako_249@hotmail.com>
2024-01-23 21:54:06 +0200

    task 1 DONE

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" Creates a class Square
+"""
+
+
+class Square:
+    """ Empty class with size attribute
+    """
+    def __init__(self, size):
+        """
+            Instantiation with size
+
+        Args:
+            size: size of the square
+        """
+        self.__size = size
