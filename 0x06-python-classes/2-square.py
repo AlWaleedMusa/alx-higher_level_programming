d8673b958f03b7166d8bf2f3a12976055d0f8be3
AlWaleed Musa <jako_249@hotmail.com>
2024-01-23 21:59:15 +0200

    refactoring code

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -6,17 +6,18 @@
 class Square:
     """ Empty class with size attribute
     """
-    def __init__(self, size):
+    def __init__(self, size=0):
         """
             Instantiation with size
 
         Args:
             size: size of the square
         """
-        self.__size = size
-
         if type(size) is not int:
             raise TypeError("size must be an integer")
         
         if size < 0:
             raise ValueError("size must be >= 0")
+        
+        self.__size = size
+
