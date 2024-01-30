97103bce30804626e7e4bb07e9bae120eb89755f
AlWaleed Musa <jako_249@hotmail.com>
2024-01-30 16:32:58 +0200

    task everything is object

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+    101-locked_class: creating a class LockedClass
+"""
+
+
+class LockedClass:
+    """
+        A class that can only have one attribute first_name.
+    
+        Attribute:
+             first_name (str): name
+    """
+    __slots__ = ['first_name']
