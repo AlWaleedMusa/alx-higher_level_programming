7a66405f64801182e2706fae14421943eb48f3b0
AlWaleed Musa <jako_249@hotmail.com>
2024-02-05 21:52:45 +0200

    task 1 DONE

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+""" Return a sorted list"""
+
+
+class MyList(list):
+    """ a subclass the inherit from list main class"""
+    
+    def print_sorted(self):
+        """Prints the list in ascending order."""
+        sorted_list = sorted(self)
+        print(sorted_list)
