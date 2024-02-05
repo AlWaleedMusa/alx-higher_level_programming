c8972b729894d919ba66ec0fa76c9d304885ff3c
AlWaleed Musa <jako_249@hotmail.com>
2024-02-05 21:42:42 +0200

    task 0 DONE + readme file

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def lookup(obj):
+	"""
+	Returns a list of available attributes and methods of an object.
+
+	Args:
+		obj: The object to inspect.
+
+	Returns:
+		List of strings representing attributes and methods.
+     """
+	return dir(obj)
