962df37ba4d4eb478b59ef2ce64f8706192c1d07
AlWaleed Musa <jako_249@hotmail.com>
2024-02-06 04:07:39 +0200

    refactoring code

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -4,19 +4,17 @@
 
 
 class BaseGeometry():
-	""" a class that raise an exception are error"""
-	def __init__(self):
-		pass
-
-	def area(self):
-		""" raise an exception"""
-		raise Exception("area() is not implemented")
-	
-	def integer_validator(self, name, value):
-		""" validate if input is a positive integer"""
-		if isinstance(value, int):
-			pass
-		else:
-			raise TypeError("{} must be an integer".format(name))
-		if value <= 0:
-			raise ValueError("{} must be greater than 0".format(name))
+    """ a class that raise an exception are error"""
+    def area(self):
+        """ raise an exception"""
+        raise Exception("area() is not implemented")
+    
+    def integer_validator(self, name, value):
+        """ validate if input is a positive integer"""
+        try:
+            if type(value) != int:
+                raise TypeError("{} must be an integer".format(name))
+            if value <= 0:
+                raise ValueError("{} must be greater than 0".format(name))
+        except Exception as e:
+            return e
