d64d085a5f32bc41618d3a85b283909be2a73910
AlWaleed Musa <jako_249@hotmail.com>
2024-02-06 18:10:13 +0200

    task 6 DONE

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+""" creates an Object from a “JSON file” """
+
+
+import json
+
+
+def load_from_json_file(filename):
+    " creates an Object from a “JSON file” "
+
+    with open(filename) as file:
+        data = file.read()
+        return json.loads(data)
