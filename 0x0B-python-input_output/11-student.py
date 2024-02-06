e2a1c697f68d5b86a5a3b1b2feb64ca933ff3664
AlWaleed Musa <jako_249@hotmail.com>
2024-02-07 00:08:07 +0200

    task 11 DONE

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+""" make a student class and return json from it attributes"""
+
+
+class Student():
+    """ make a student class"""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return self.__dict__.copy()
+        else:
+            result = {}
+            for attr in attrs:
+                if hasattr(self, attr):
+                    result[attr] = getattr(self, attr)
+            return result
+
+    def reload_from_json(self, json):
+        with open(json, "r") as file:
+            data = file.readline()
+            return data
