cbd641508a688697156d41e0f92d1aa99175e374
AlWaleed Musa <jako_249@hotmail.com>
2024-02-06 23:06:15 +0200

    task 8 DONE

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+""" returns the dictionary description with simple data structure """
+
+
+def class_to_json(obj):
+    """ Function that return the dictionary description of an obj """
+
+    result = {}
+    if hasattr(obj, "__dict__"):
+        result = obj.__dict__.copy()
+
+    return result
