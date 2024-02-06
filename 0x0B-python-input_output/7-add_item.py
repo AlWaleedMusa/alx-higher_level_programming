9c2c1442312360323243de9c82a8f7ee297d824f
AlWaleed Musa <jako_249@hotmail.com>
2024-02-06 19:08:04 +0200

    refactoring code

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,30 +3,18 @@
 """ adds all arguments to a Python list, and then save them to a file """
 
 
-import json
 import sys
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-
-def main():
-    """ entry point"""
-
-    args_list = sys.argv[1:]
-    save_to_json_file(args_list, "add_item.json")
-    print(load_from_json_file("add_item.json"))
-
-def save_to_json_file(my_obj, filename):
-    """ writes an Object to a text file, using a JSON representation """
-
-    with open(filename, "a") as file:
-        json_str = json.dumps(my_obj)
-        file.write(json_str)
-
-def load_from_json_file(filename):
-    " creates an Object from a “JSON file” "
-
-    with open(filename) as file:
-        data = file.read()
-        return json.loads(data)
 
 if __name__ == "__main__":
-    main()
+
+    try:
+        args = load_from_json_file("add_item.json")
+    except:
+        args = []
+
+    for arg in sys.argv[1:]:
+        args.append(arg)
+    save_to_json_file(args, "add_item.json")
