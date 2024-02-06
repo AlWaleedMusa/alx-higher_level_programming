6bfe987a31e9081cace6b57e54616deb3b28bcdc
AlWaleed Musa <jako_249@hotmail.com>
2024-02-06 17:56:04 +0200

    pycode styling

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -7,6 +7,6 @@ import json
 
 
 def from_json_string(my_str):
-	""" return a python struct from a json str"""
+    """ return a python struct from a json str"""
 
-	return json.loads(my_str)
+    return json.loads(my_str)
