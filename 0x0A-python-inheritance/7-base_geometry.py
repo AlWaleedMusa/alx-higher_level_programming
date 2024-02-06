f0768f2b5973e42bf0843658d4ad6228df0399c8
AlWaleed Musa <jako_249@hotmail.com>
2024-02-06 04:16:05 +0200

    pycode styling

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -9,7 +9,7 @@ class BaseGeometry():
     def area(self):
         """ raise an exception"""
         raise Exception("area() is not implemented")
-    
+
     def integer_validator(self, name, value):
         """Validate name and value
         Args:
