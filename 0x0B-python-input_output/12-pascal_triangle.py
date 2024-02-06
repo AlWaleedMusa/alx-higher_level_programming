4d40ba636c20518d0f196b9d3ffd9b22ad4fd5f2
AlWaleed Musa <jako_249@hotmail.com>
2024-02-07 00:19:17 +0200

    task 12 DONE

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+""" pascal triangle """
+
+def pascal_triangle(n):
+    """ return a pascal triangle as a list"""
+
+    triangle = []
+
+    for i in range(n):
+        row = [1]
+        if i > 0:
+            prev_row = triangle[i - 1]
+            for j in range(1, i):
+                row.append(prev_row[j - 1] + prev_row[j])
+            row.append(1)
+        triangle.append(row)
+
+    return triangle
