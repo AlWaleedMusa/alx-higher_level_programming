25cb9ef0adda6690dd7825331d5cebc59973c981
AlWaleed Musa <jako_249@hotmail.com>
2024-02-07 03:48:06 +0200

    pycode styling

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -4,16 +4,16 @@
 
 
 def append_after(filename="", search_string="", new_string=""):
-	""" inserts a line of text to a file, after each line containing a specific string """
+    """ inserts a line of text to a file, after each line containing a specific string """
 
-	new_list = []
-	with open(filename, "r") as file:
-		data = file.readlines()
+    new_list = []
+    with open(filename, "r") as file:
+        data = file.readlines()
 
-		for line in data:
-			new_list.append(line)
-			if search_string in line:
-				new_list.append(new_string)
+        for line in data:
+            new_list.append(line)
+            if search_string in line:
+                new_list.append(new_string)
 
-	with open(filename, "w") as file:
-		file.writelines(new_list)
+    with open(filename, "w") as file:
+        file.writelines(new_list)
