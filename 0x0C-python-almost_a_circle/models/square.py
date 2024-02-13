110a1de373a8bbf96eae919f849337eaa3cf3aef
AlWaleed Musa <jako_249@hotmail.com>
2024-02-13 04:11:07 +0200

    refactoring code

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -6,14 +6,14 @@ from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
+    """ Initialize a new Square.
+        Args:
+            size (int): size of the new Square.
+            x (int): x coordinate of the new Square.
+            y (int): y coordinate of the new Square.
+            id (int): identity of the new Square.
+    """
     def __init__(self, size, x=0, y=0, id=None):
-        """ Initialize a new Square.
-            Args:
-                size (int): size of the new Square.
-                x (int): x coordinate of the new Square.
-                y (int): y coordinate of the new Square.
-                id (int): identity of the new Square.
-        """
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
