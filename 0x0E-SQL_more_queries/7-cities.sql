e8efa29c52af5eba448da8a633b8d95a63d2cf45
AlWaleed Musa <jako_249@hotmail.com>
2024-03-14 02:34:45 +0200

    task 7 done

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,6 +1,9 @@
 -- creates the database hbtn_0d_usa and the table cities(in the database hbtn_0d_usa)
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
+
+USE hbtn_0d_usa;
+
+CREATE TABLE IF NOT EXISTS cities (
 	id PRIMARY KEY AUTO_INCREMENT,
 	state_id INT NOT NULL,
 	name VARCHAR(256) NOT NULL,
