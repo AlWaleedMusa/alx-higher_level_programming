62416c5d3e4b04afda5ec7e46c379483c6dfab6d
Waleed Musa <jako_249@hotmail.com>
2024-04-08 20:49:51 +0200

    task 10 const added

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-function add(a, b) {
-    return a + b
+function add (a, b) {
+  return parseInt(a) + parseInt(b);
 }
-process = require('process')
-console.log(add(parseInt(process.argv[2]), parseInt(process.argv[3])))
+const process = require('process');
+console.log(add(process.argv[2], process.argv[3]));
