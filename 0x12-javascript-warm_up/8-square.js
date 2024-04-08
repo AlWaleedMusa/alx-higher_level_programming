401dca91c2d816869b223e318e8cf3019418fb2a
Waleed Musa <jako_249@hotmail.com>
2024-04-08 07:04:43 +0200

    task 9 done

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,15 @@
+#!/user/bin/node
+
+const process = require('process');
+
+if (parseInt(process.argv[2])) {
+  const len = parseInt(process.argv[2]);
+  for (let i = 0; i < len; i++) {
+    for (let j = 0; j < len; j++) {
+      process.stdout.write('X');
+    }
+    console.log();
+  }
+} else {
+  console.log('Missing size');
+}
