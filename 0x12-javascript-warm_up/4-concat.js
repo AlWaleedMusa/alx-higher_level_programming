355c02ed97529f5b8d7c727690ea32e993e6a583
Waleed Musa <jako_249@hotmail.com>
2024-04-08 06:23:03 +0200

    task 5 done

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+const process = require('process');
+
+if (process.argv) {
+  const var1 = process.argv[2];
+  const var2 = process.argv[3];
+  console.log(`${var1} is ${var2}`);
+}
