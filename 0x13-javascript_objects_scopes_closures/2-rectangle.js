063d4d701d39deada3c1fef40677959b782a2272
Waleed Musa <jako_249@hotmail.com>
2024-04-09 21:31:55 +0200

    task 2 fixed issue with return

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
 class Rectangle {
   constructor (w, h) {
-    if (w <= 0 || h <= 0) {
-      return this;
-    } else {
+    if (w > 0 && typeof w === 'number' && h > 0 && typeof h === 'number') {
       this.width = w;
       this.height = h;
     }
