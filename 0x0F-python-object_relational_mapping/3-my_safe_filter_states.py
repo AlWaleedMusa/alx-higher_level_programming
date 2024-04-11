972821cf27cf07cf5d54352cb676f4400f77aebb
Waleed Musa <jako_249@hotmail.com>
2024-04-11 19:07:48 +0200

    task 3 done

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""displays all values in the states table of hbtn_0e_0_usa where name matches the argument and be safe"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost",
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        port=3306,
+    )
+
+    search = sys.argv[4]
+
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE %s", (search,))
+    data = cur.fetchall()
+    for row in data:
+        print(row)
+    cur.close()
+    db.close()
