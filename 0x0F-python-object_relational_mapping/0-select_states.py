1c2344373004d4268101bcc4e3fdbe0eeb340c4e
Waleed Musa <jako_249@hotmail.com>
2024-04-11 18:27:52 +0200

    task 2 done + fixed white space in all tasks

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -4,7 +4,6 @@
 import MySQLdb
 import sys
 
-
 if __name__ == "__main__":
     db = MySQLdb.connect(
         host="localhost",
--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""  lists all states with a name starting with (upper N) database hbtn_0e_0_usa """
+"""displays all values in the states table of hbtn_0e_0_usa where name matches the argument"""
 
 import MySQLdb
 import sys
@@ -13,11 +13,12 @@ if __name__ == "__main__":
         port=3306,
     )
 
-    crs = db.cursor()
-    crs.execute("SELECT * FROM states WHERE BINARY name LIKE 'N%' ORDER BY states.id")
-    data = crs.fetchall()
+    search = sys.argv[4]
 
+    cur = db.cursor()
+    cur.execute('SELECT * FROM states WHERE name="{}"'.format(search))
+    data = cur.fetchall()
     for row in data:
         print(row)
-    crs.close()
+    cur.close()
     db.close()
