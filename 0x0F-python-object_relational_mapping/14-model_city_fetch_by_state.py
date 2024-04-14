504ae65b024a00958363fc11576a7b4c90429441
Jako <136924902+AlWaleedMusa@users.noreply.github.com>
2024-04-14 07:17:43 +0200

    task 14 pycode style fix

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -21,6 +21,5 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    for city, state in session.query(
-        City, State).filter(City.state_id == State.id).all():
+    for city, state in session.query(City, State).filter(City.state_id == State.id).all():
         print("{}: ({}) {}".format(state.name, city.id, city.name))
