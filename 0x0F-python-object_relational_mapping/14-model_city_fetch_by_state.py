6f5d77307554e5b2d4ca806f1a8af3fd56a1caa4
Jako <136924902+AlWaleedMusa@users.noreply.github.com>
2024-04-14 07:12:25 +0200

    task 14 pycode style fix

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-prints all City objects from the database hbtn_0e_14_usa
+prints all City objects from
+the database hbtn_0e_14_usa
 """
 
 import sys
@@ -13,10 +14,14 @@ from model_city import City
 if __name__ == "__main__":
     engine = create_engine(
         "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
-            sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+            sys.argv[1],
+            sys.argv[2],
+            sys.argv[3]),
+            pool_pre_ping=True)
     
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    for city, state in session.query(City, State).filter(City.state_id == State.id).all():
+    for city, state in session.query(City, State).filter(
+        City.state_id == State.id).all():
         print("{}: ({}) {}".format(state.name, city.id, city.name))
