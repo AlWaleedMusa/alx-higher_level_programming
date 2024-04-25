7377b901d1090e0f63ae356273b59e5bd0a79da0
Jako <136924902+AlWaleedMusa@users.noreply.github.com>
2024-04-25 17:55:33 +0200

    task 0 fix

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
 # script that takes in a URL and displays the size of the body of the response
-curl -sI "$1" | grep "Content-Length" | cut -d " " -f2
+curl -s "$1" | grep "Content-Length" | cut -d " " -f2
