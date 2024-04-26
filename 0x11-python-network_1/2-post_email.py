93bf7e86dc445aa4a64c71e1a62d4f9cfb065716
Jako <136924902+AlWaleedMusa@users.noreply.github.com>
2024-04-26 23:56:40 +0300

    task 2 done

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Send a POST request with an email"""
+
+import urllib.parse
+import urllib.request
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+    values = {"email": email}
+    data = urllib.parse.urlencode(values)
+    data = data.encode("ascii")
+    req = urllib.request.Request(url, data)
+    with urllib.request.urlopen(req) as response:
+        print(response.read().decode("utf-8"))
