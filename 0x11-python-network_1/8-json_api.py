bafe235b19fc0a61c3844a28c56f3ee9ac04fd57
Jako <136924902+AlWaleedMusa@users.noreply.github.com>
2024-04-27 01:56:44 +0300

    task 8 done

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+# script that takes in a letter and sends a POST request with a parameter
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = "http://0.0.0.0:5000/search_user"
+    q = sys.argv[1] if len(sys.argv) > 1 else ""
+    q = {"q": q}
+    response = requests.post(url, data=q)
+
+    try:
+        response = response.json()
+
+        if response:
+            print("[{}] {}".format(response.get("id"), response.get("name")))
+        else:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")
