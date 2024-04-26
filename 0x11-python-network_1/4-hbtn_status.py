4137d8d5a4ff799c521cb94c3b1365938d0b7e1b
Jako <136924902+AlWaleedMusa@users.noreply.github.com>
2024-04-27 00:19:14 +0300

    task 4 done

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Fetches a URL using request module"""
+
+import requests
+
+if __name__ == "__main__":
+    url = "https://alx-intranet.hbtn.io/status"
+    response = requests.get(url)
+    print("Body response:")
+    print("\t- type: {}".format(type(response.content)))
+    print("\t- content: {}".format(response.content.decode("utf-8")))
