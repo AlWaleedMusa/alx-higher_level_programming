63c402cdde2825b8cf8a05e50a0fd1720b54b1e5
Jako <136924902+AlWaleedMusa@users.noreply.github.com>
2024-05-21 10:06:24 +0300

    task 5 done

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+// script that gets the contents of a webpage and stores it in a file
+
+const request = require('request');
+const fs = require('fs');
+
+const url = process.argv[2];
+
+if (!url) {
+    console.error('Please provide a URL as the first argument.');
+    process.exit(1);
+    }
+
+request.get(url, (error, response, body) => {
+    if (error) {
+        console.error('Error reading file:', error);
+        } else {
+            fs.writeFile(process.argv[3], body, 'utf8', (err) => {
+                if (err) {
+                    console.error('Error writing to file:', err);
+                    }
+                });
+        }
+    }
+);
