3c902113e64d5e4e4c200a5c04dd4feaa84206bf
Jako <136924902+AlWaleedMusa@users.noreply.github.com>
2024-05-21 11:30:00 +0300

    task 6 done

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+// script that computes the number of tasks completed by user id.
+
+const request = require('request');
+const url = "https://jsonplaceholder.typicode.com/todos";
+
+request.get(url, function (error, response, body) {
+    if (error) {
+        console.error('Error reading file:', error);
+    }
+    const todos = JSON.parse(body);
+    const completed = {};
+    for (const todo of todos) {
+        if (todo.completed) {
+            if (completed[todo.userId]) {
+                completed[todo.userId]++;
+            } else {
+                completed[todo.userId] = 1;
+            }
+        }
+    }
+    console.log(completed);
+    }
+);
