458924aa66a18751bcf4f8a5896037a0502bd2fe
Jako <136924902+AlWaleedMusa@users.noreply.github.com>
2024-05-21 09:54:05 +0300

    task 2 done

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// script that prints the status code of a get request
+
+const request = require('request');
+const url = process.argv[2];
+
+request.get(url).on('response', function (response) {
+    console.log('code:', response.statusCode);
+    });
